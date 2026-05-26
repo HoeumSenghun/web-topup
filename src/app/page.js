32c272738500f4f35389245682a171b1df8e9e23
HoeumSenghun <hoeumsenghun369@gmail.com>
2026-05-26 08:43:06 +0700

    config tailwindcss

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <main>
-        <h1>Welcome to Diamond Shop</h1>
+        <h1 className="text-2xl font-medium underline">Welcome to Diamond Shop</h1>
       </main>
     </div>
   );
